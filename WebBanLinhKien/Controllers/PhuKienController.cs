dfe6bb2c45daaf1bd29b9782a9aaf69453230f45
trongnhan3004 <hoangungdai@gmail.com>
2021-06-25 16:47:01 +0700

    ok moi lam chuc nang suong suong

---
+++ WebBanLinhKien/Controllers/PhuKienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanLinhKien.Controllers
+{
+    public class PhuKienController : Controller
+    {
+        // GET: PhuKien
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
